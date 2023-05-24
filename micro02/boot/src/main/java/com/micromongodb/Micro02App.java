a4e07210d5d94eb7c179e777dfa429a73ce65e1a
jdiavera <jdiavera@emeal.nttdata.com>
2023-05-24 03:01:18 +0200

    a

--- micro02/boot/src/main/java/com/micromongodb/Micro02App.java
+++ micro02/boot/src/main/java/com/micromongodb/Micro02App.java
@@ -2,8 +2,10 @@ package com.micromongodb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class Micro02App {
     public static void main(String[] args) {
         SpringApplication.run(Micro02App.class,args);
--- micromongodb/boot/src/main/java/com/fomation/micromongo/Micro01.java
+++ micromongodb/boot/src/main/java/com/fomation/micromongo/Micro01.java
@@ -3,9 +3,11 @@ package com.fomation.micromongo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class Micro01 {
 
 	public static void main(String[] args) {
